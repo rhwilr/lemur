929bfe75eb31bf0ca4d746c516c7616cd6cedc94
Ralph Huwiler <ralph@huwiler.dev>
2019-10-15 22:36:48 +0200

    added the first few tokens to the lexer

---
+++ src/monkey/token/token.go
@@ -0,0 +1,36 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	// Special types
+	ILLEGAL = "ILLEGAL" // Used when we encounter an unknown token
+	EOF     = "EOF"     // Used to signal the end of a file to the parser
+
+	// Indentifiers and literals
+	IDENT = "IDENT" // Identifier like variable names
+	INT   = "INT"   // Integers 1, 2,3, 42...
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+
+	// Delimiters
+	COMMA      = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
