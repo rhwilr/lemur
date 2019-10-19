772771ca2dc62aeb2935b8ac752127fa4d35d395
Ralph <ralph@huwiler.dev>
2019-10-19 16:46:56 +0200

    wip: implemented the evaluator for integers and booleans

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey/lexer"
 	"monkey/parser"
+	"monkey/evaluator"
 )
 
 const PROMPT = ">> "
@@ -31,8 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
