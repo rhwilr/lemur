e7702ce5c5c6e443245fe991eab120aa431ae4a3
Ralph Huwiler <ralph@huwiler.dev>
2019-10-21 12:47:31 +0200

    (4.4) implemented builtin functions for array

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -360,19 +360,20 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len("hello world")`, 11},
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
-		// {`len([1, 2, 3])`, 3},
-		// {`len([])`, 0},
+		{`len([1, 2, 3])`, 3},
+		{`len([])`, 0},
 		// {`puts("hello", "world!")`, nil},
-		// {`first([1, 2, 3])`, 1},
-		// {`first([])`, nil},
-		// {`first(1)`, "argument to `first` must be ARRAY, got INTEGER"},
-		// {`last([1, 2, 3])`, 3},
-		// {`last([])`, nil},
-		// {`last(1)`, "argument to `last` must be ARRAY, got INTEGER"},
-		// {`rest([1, 2, 3])`, []int{2, 3}},
-		// {`rest([])`, nil},
-		// {`push([], 1)`, []int{1}},
-		// {`push(1, 1)`, "argument to `push` must be ARRAY, got INTEGER"},
+		{`first([1, 2, 3])`, 1},
+		{`first([])`, nil},
+		{`first(1)`, "argument to `first` must be ARRAY, got INTEGER"},
+		{`last([1, 2, 3])`, 3},
+		{`last([])`, nil},
+		{`last(1)`, "argument to `last` must be ARRAY, got INTEGER"},
+		{`rest([1, 2, 3])`, []int{2, 3}},
+		{`rest([])`, nil},
+		{`rest(1)`, "argument to `rest` must be ARRAY, got INTEGER"},
+		{`push([], 1)`, []int{1}},
+		{`push(1, 1)`, "argument to `push` must be ARRAY, got INTEGER"},
 	}
 
 	for _, tt := range tests {
@@ -394,22 +395,22 @@ func TestBuiltinFunctions(t *testing.T) {
 				t.Errorf("wrong error message. expected=%q, got=%q",
 					expected, errObj.Message)
 			}
-			// case []int:
-			// 	array, ok := evaluated.(*object.Array)
-			// 	if !ok {
-			// 		t.Errorf("obj not Array. got=%T (%+v)", evaluated, evaluated)
-			// 		continue
-			// 	}
+			case []int:
+				array, ok := evaluated.(*object.Array)
+				if !ok {
+					t.Errorf("obj not Array. got=%T (%+v)", evaluated, evaluated)
+					continue
+				}
 
-			// 	if len(array.Elements) != len(expected) {
-			// 		t.Errorf("wrong num of elements. want=%d, got=%d",
-			// 			len(expected), len(array.Elements))
-			// 		continue
-			// 	}
+				if len(array.Elements) != len(expected) {
+					t.Errorf("wrong num of elements. want=%d, got=%d",
+						len(expected), len(array.Elements))
+					continue
+				}
 
-			// 	for i, expectedElem := range expected {
-			// 		testIntegerObject(t, array.Elements[i], int64(expectedElem))
-			// 	}
+				for i, expectedElem := range expected {
+					testIntegerObject(t, array.Elements[i], int64(expectedElem))
+				}
 		}
 	}
 }
